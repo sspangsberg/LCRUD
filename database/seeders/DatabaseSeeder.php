a28276a36238b8c5c409ab4e469409c1a80bf201
Spangsberg <smsj@Spangsbergs-MBP.local>
2022-01-19 12:22:01 +0100

    Finished

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,6 +13,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        //$this->call(ProductSeeder::class);
+        \App\Models\Product::factory(50)->create();
+        \App\Models\User::factory(10)->create();
     }
 }
--- resources/views/products/layout.blade.php
+++ resources/views/products/layout.blade.php
@@ -5,7 +5,6 @@
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css" integrity="sha384-zCbKRCUGaJDkqS1kPbPd7TveP5iyJE0EjAuZQTgFLD2ylzuqKfdKlfG/eSrtxUkn" crossorigin="anonymous">
     </head>
     <body>
-        <h1>Laravel CRUD 8 Application</h1>
         <div class="container">
             <br>
             @yield('content')
